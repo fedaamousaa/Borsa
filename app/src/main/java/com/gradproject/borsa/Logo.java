c055958325f70816bd89769db2af7f888eca8201
dreams4ever <sabersa14@yahoo.com>
2016-12-28 20:50:06 +0200

    change pictures and modify time of  logo

--- app/src/main/java/com/gradproject/borsa/Logo.java
+++ app/src/main/java/com/gradproject/borsa/Logo.java
@@ -1,6 +1,8 @@
 package com.gradproject.borsa;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class Logo extends AppCompatActivity {
@@ -10,6 +12,13 @@ public class Logo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
 
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                //start new activity here
+                Intent in = new Intent(Logo.this, guides.class);
+                startActivity(in);
+            }
+        }, 7000);
 
 
     }
--- app/src/main/java/com/gradproject/borsa/guides.java
+++ app/src/main/java/com/gradproject/borsa/guides.java
@@ -1,13 +1,38 @@
 package com.gradproject.borsa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class guides extends AppCompatActivity {
-
+TextView skip;
+    Button buttons;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guides);
+
+        skip=(TextView)findViewById(R.id.skip);
+        buttons=(Button)findViewById(R.id.buttons);
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(guides.this, MarketOverView.class);
+                startActivity(in);
+            }
+        });
+
+
+        buttons.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(guides.this, MainActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
